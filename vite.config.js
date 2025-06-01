1ff7b436991784378c5e66f49081f6ab856c6e3b
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 11:03:34 +0100

    fix(vite): update base URL for local development

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://guilhermeleitao2002.github.io/movie-dashboard/'
+  base: '/movie-dashboard/'
 })
