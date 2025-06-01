93459e143da9a7565f6c7e87838f75162ce0e47e
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 11:01:01 +0100

    feat(vite): update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-dashboard/'
+  base: 'https://guilhermeleitao2002.github.io/movie-dashboard/'
 })
